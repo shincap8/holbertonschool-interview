56b1f6a779466133094e3658557028efbb3759b6
shincap8 <shincap8@gmail.com>
2020-08-07 15:23:30 +0000

    Interview task

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    if type(n) is not int or n <= 0:
+        return 0
+    nH = 1
+    copy = 1
+    operations = 1
+    while nH < n:
+        if n % nH == 0:
+            if nH != copy:
+                copy = nH
+                operations += 1
+        nH = nH + copy
+        operations += 1
+    if nH == n:
+        return operations
+    return 0
