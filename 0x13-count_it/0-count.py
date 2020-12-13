0d1133a214fdae1dbb77f6e70ef4dd64466ddac9
shincap8 <shincap8@gmail.com>
2020-12-13 19:26:12 +0000

    Try with others code

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
-""" ecursive function that queries the Reddit API, parses the title
-of all hot articles, and prints a sorted count of given keywords
 """
-from requests import get
+Recursive function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit
+"""
+
+import operator
+import requests
 
 
-def count_words(subreddit, word_list, after="", counter={}, first=0):
-    """parses the title of all hot articles, and prints a sorted list."""
-    url = 'https://www.reddit.com'
-    path = '/r/{}/hot.json?after='.format(subreddit)
-    header = {'user-agent': 'jormao'}
-    r = get(url + path + str(after), headers=header, allow_redirects=False)
-    if (first == 0):
+def count_words(subreddit, word_list, dict_list={}, after=''):
+    url = 'https://www.reddit.com/r/'+subreddit+'/hot.json?after='+after
+    json = requests.get(url, headers={'user-agent': 'Mozilla/5.0'}).json()
+
+    if 'error' in json or json['data']['children'] == []:
+        return None
+
+    if len(dict_list) == 0:
         for word in word_list:
-            counter[word] = 0
-        first = 1
-    if (r.status_code in [302, 404]):
-        return(None)
-    else:
-        hot_post = r.json()['data']['children']
-        for hot in hot_post:
-            new_list = (hot['data']['title']).lower().split(" ")
+            dict_list[word] = 0
+
+    children = json['data']['children']
+    for child in children:
+        tokens = child['data']['title'].upper()
+        tokens = tokens.split()
+        for token in tokens:
             for word in word_list:
-                for w_list in new_list:
-                    if word.lower() == w_list:
-                        counter[word] += 1
-        after = r.json()['data']['after']
-        if (after is not None):
-            count_words(subreddit, word_list, after, counter, first)
-        else:
-            p = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-            for k, v in p:
-                if v != 0:
-                    print('{}: {}'.format(k, v))
+                if token == word.upper():
+                    dict_list[word] += 1
+    after = json['data']['after']
+    if after is None:
+        sorted_dict = sorted(dict_list.items(), key=operator.itemgetter(1),
+                             reverse=True)
+        ordered = False
+        size = len(sorted_dict)
+        while ordered is False:
+            ordered = True
+            for i in range(size):
+                if (i < size - 1):
+                    if (sorted_dict[i][1] == sorted_dict[i+1][1] and
+                            sorted_dict[i][0] > sorted_dict[i+1][0]):
+                        ordered = False
+                        aux = sorted_dict[i]
+                        sorted_dict[i] = sorted_dict[i+1]
+                        sorted_dict[i+1] = aux
+
+        for value in sorted_dict:
+            if value[1] > 0:
+                print("{}: {}".format(value[0], value[1]))
+    else:
+        count_words(subreddit, word_list, dict_list, after)
