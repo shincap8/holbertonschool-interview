f6102f2841b666ee5722ee93b6020a511ea9bac8
shincap8 <shincap8@gmail.com>
2021-01-10 19:29:56 +0000

    Correct Betty

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -4,5 +4,5 @@ int _putchar(char c);
 void init(char *str, int l);
 int sonnum(char **av);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
-void _print(char *str, int l)
+void _print(char *str, int l);
 #endif
