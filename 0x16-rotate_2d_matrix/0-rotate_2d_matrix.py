1bea96302fa264af25d46c6e56bbc0f26dd2f109
shincap8 <shincap8@gmail.com>
2021-01-15 20:44:00 +0000

    Interiew Task

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Method to rotate a 2Dmatrix, 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """Method to rotate a 2Dmatrix, 90 degrees clockwise"""
+    if len(matrix) < 2:
+        return
+    n = len(matrix)
+    print(len(matrix))
+    for i in range(0, int(n / 2)):
+        for j in range(i, n - i - 1):
+            """print("rango: ", n - i - 1)
+            print("i: {}, j: {}".format(i, j))"""
+            aux = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = aux
+    return
