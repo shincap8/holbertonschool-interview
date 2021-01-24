951f1f4023c9757fc5706eff40493790332fcf34
shincap8 <shincap8@gmail.com>
2021-01-24 21:22:41 +0000

    Try two steps ahead

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -11,15 +11,32 @@
 */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *fast;
+	listint_t *slow;
+
 	if (head == NULL)
 		return (NULL);
-	while (head)
+
+	slow = head;
+	fast = head;
+
+	while (slow && fast && fast->next)
 	{
-		if ((head->next) >= head)
+		fast = fast->next;
+		fast = fast->next;
+		slow = slow->next;
+
+		if (slow == fast && slow == head)
 		{
-			return (head->next);
+			return (slow);
 		}
-		head = head->next;
+		else if (slow == fast)
+		{
+			slow = find_listint_loop(head->next);
+			if (slow)
+				return (slow);
+		}
 	}
+
 	return (NULL);
 }
