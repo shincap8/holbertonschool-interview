a3b152333a3aa0e5d2377eef5c02f5098a5427f2
shincap8 <shincap8@gmail.com>
2021-01-25 17:57:36 +0000

    The turtle and the rabbit

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,28 +1,43 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdarg.h>
-#include <stdio.h>
+#include "lists.h"
 /**
-* find_listint_loop- finds the loop in the linked list
-* @head: pointer to the first element
-*
-* Description: this function finds a loop in the linked list
-* Return: the address of the node where the loop starts
-*/
+ * loop - check through the list.
+ * @turtle: pointer one by one
+ * @rabbit: pointer one ahead
+ * Return: pointer where the loop start
+ */
+listint_t *loop(listint_t *turtle, listint_t *rabbit)
+{
+	while (turtle != rabbit)
+	{
+		turtle = turtle->next;
+		rabbit = rabbit->next;
+	}
+	return (turtle);
+}
+/**
+ * find_listint_loop - finds loops inside linked lists
+ * @head: pointer to the first element
+ * Return: the address of the node where the loop starts
+ */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *last = head;
+	listint_t *turtle, *rabbit;
 
-	while (head != NULL && last > head->next)
+	if (head)
 	{
-		last = head;
-		head = head->next;
+		turtle = head->next;
+		rabbit = head->next->next;
+		while (turtle && rabbit)
+		{
+			if (turtle == rabbit)
+			{
+				turtle = head;
+				return (loop(turtle, rabbit));
+			}
+			turtle = turtle->next;
+			rabbit = rabbit->next->next;
+		}
 	}
-	if (head != NULL)
-	{
-		head = head->next;
-		return (head);
-	}
-	else
-		return (NULL);
+	return (NULL);
 }
