077d3453e163eed99b575a26eafba9b37ca9889e
shincap8 <shincap8@gmail.com>
2021-02-14 20:20:26 +0000

    use less functions

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,95 +1,57 @@
 #include "sort.h"
 /**
- * print_split - Prints an array of integers
- *
- * @array: The array to be printed
- * @start: start of array to print
- * @end: end of array to print
- */
-void print_split(const int *array, size_t start, size_t end)
+ * merge - merges l and r arrays into original array
+ * @array: pointer to array
+ * @size: size of the array
+ * @L: pointer to left array
+ * @R: pointer to right array
+ **/
+void merge(int *array, int *L, int *R, size_t size)
 {
-	size_t i;
+	int i = 0, j = 0, k = 0;
+	int size_l, size_r;
 
-	for (i = start; i < end; i++)
-	{
-		if (i > start)
-			printf(", ");
-		printf("%d", array[i]);
-	}
-	printf("\n");
-}
-/**
-* top_down_merge - merge the splited array
-* @B: source array
-* @start: start of the array to split
-* @middle: middle of the array to merge
-* @end: end of the array to split
-* @A: array to be sorte
-*/
-void top_down_merge(int *A, size_t start, size_t middle, size_t end, int *B)
-{
-	size_t s = start, m = middle, i;
-
+	size_l = size / 2;
+	size_r = size - size_l;
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_split(A, start, middle);
+	print_array(L, size_l);
 	printf("[right]: ");
-	print_split(A, middle, end);
-	for (i = start; i < end; i++)
+	print_array(R, size_r);
+	while (i < size_l && j < size_r)
 	{
-		if (s < middle && (m >= end || A[s] <= A[m]))
-		{
-			B[i] = A[s];
-			s  = s + 1;
-		}
+		if (L[i] < R[j])
+			array[k++] = L[i++];
 		else
-		{
-			B[i] = A[m];
-			m = m + 1;
-		}
+			array[k++] = R[j++];
 	}
+	while (i < size_l)
+		array[k++] = L[i++];
+	while (j < size_r)
+		array[k++] = R[j++];
 	printf("[Done]: ");
-	print_split(B, start, end);
+	print_array(array, size);
 }
 /**
-* top_down_split - split the array
-* @B: source array
-* @start: start of the array to split
-* @end: end of the array to split
-* @A: array to be sorte
-*/
-void top_down_split(int *B, size_t start, size_t end, int *A)
-{
-	size_t middle;
-
-	if (end - start <= 1)
-		return;
-	middle = (start + end) / 2;
-	top_down_split(A, start, middle, B);
-	top_down_split(A, middle, end, B);
-	top_down_merge(B, start, middle, end, A);
-}
-/**
-* merge_sort - Sort algorithm using top-down
-* merge sort algorithm
-* @array: sorting array
-* @size: length array
-*/
+ * merge_sort - sorts an array of integers using
+ * the Merge sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void merge_sort(int *array, size_t size)
 {
-	int *B;
-	size_t i;
+	size_t middle = 0, i;
+	int L[size];
+	int R[size];
 
 	if (array == NULL || size < 2)
 		return;
-
-	B = malloc(sizeof(int) * size);
-	if (B == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		B[i] = array[i];
-	}
-	top_down_split(B, 0, 10, array);
-	free(B);
+	middle = size / 2;
+	for (i = 0; i < middle; i++)
+		L[i] = array[i];
+	for (i = middle; i < size; i++)
+		R[i - middle] = array[i];
+	merge_sort(L, middle);
+	merge_sort(R, size - middle);
+	merge(array, L, R, size);
 }
