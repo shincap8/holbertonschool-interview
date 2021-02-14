9099284689f9f4fc76b5ba27b5c1c47c95fdb196
shincap8 <shincap8@gmail.com>
2021-02-14 20:00:41 +0000

    remove malloc again

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -77,10 +77,13 @@ void top_down_split(int *B, size_t start, size_t end, int *A)
 */
 void merge_sort(int *array, size_t size)
 {
-	int *B;
+	int B[size];
 	size_t i;
 
-	B = malloc(sizeof(int) * size);
+	if (!array)
+		return;
+	if (size < 2)
+		return;
 	if (B == NULL)
 		return;
 	for (i = 0; i < size; i++)
@@ -88,5 +91,4 @@ void merge_sort(int *array, size_t size)
 		B[i] = array[i];
 	}
 	top_down_split(B, 0, 10, array);
-	free(B);
 }
