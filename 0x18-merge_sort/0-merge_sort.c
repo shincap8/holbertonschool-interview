35fe35a7b840fb5c0cf86551888c53eb4fb0bd39
shincap8 <shincap8@gmail.com>
2021-02-14 20:25:19 +0000

    Use of number instead of variable on malloc

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,57 +1,92 @@
 #include "sort.h"
 /**
- * merge - merges l and r arrays into original array
- * @array: pointer to array
- * @size: size of the array
- * @L: pointer to left array
- * @R: pointer to right array
- **/
-void merge(int *array, int *L, int *R, size_t size)
+ * print_split - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @start: start of array to print
+ * @end: end of array to print
+ */
+void print_split(const int *array, size_t start, size_t end)
 {
-	int i = 0, j = 0, k = 0;
-	int size_l, size_r;
-
-	size_l = size / 2;
-	size_r = size - size_l;
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(L, size_l);
-	printf("[right]: ");
-	print_array(R, size_r);
-	while (i < size_l && j < size_r)
+	size_t i;
+	for (i = start; i < end; i++)
 	{
-		if (L[i] < R[j])
-			array[k++] = L[i++];
-		else
-			array[k++] = R[j++];
+		if (i > start)
+			printf(", ");
+		printf("%d", array[i]);
 	}
-	while (i < size_l)
-		array[k++] = L[i++];
-	while (j < size_r)
-		array[k++] = R[j++];
-	printf("[Done]: ");
-	print_array(array, size);
+	printf("\n");
 }
 /**
- * merge_sort - sorts an array of integers using
- * the Merge sort algorithm
- * @array: pointer to array
- * @size: size of the array
- **/
+* top_down_merge - merge the splited array
+* @B: source array
+* @start: start of the array to split
+* @middle: middle of the array to merge
+* @end: end of the array to split
+* @A: array to be sorte
+*/
+void top_down_merge(int *A, size_t start, size_t middle, size_t end, int *B)
+{
+	size_t s = start, m = middle, i;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_split(A, start, middle);
+	printf("[right]: ");
+	print_split(A, middle, end);
+	for (i = start; i < end; i++)
+	{
+		if (s < middle && (m >= end || A[s] <= A[m]))
+		{
+			B[i] = A[s];
+			s  = s + 1;
+		}
+		else
+		{
+			B[i] = A[m];
+			m = m + 1;
+		}
+	}
+	printf("[Done]: ");
+	print_split(B, start, end);
+}
+/**
+* top_down_split - split the array
+* @B: source array
+* @start: start of the array to split
+* @end: end of the array to split
+* @A: array to be sorte
+*/
+void top_down_split(int *B, size_t start, size_t end, int *A)
+{
+	size_t middle;
+	if (end - start <= 1)
+		return;
+	middle = (start + end) / 2;
+	top_down_split(A, start, middle, B);
+	top_down_split(A, middle, end, B);
+	top_down_merge(B, start, middle, end, A);
+}
+/**
+* merge_sort - Sort algorithm using top-down
+* merge sort algorithm
+* @array: sorting array
+* @size: length array
+*/
 void merge_sort(int *array, size_t size)
 {
-	size_t middle = 0, i;
-	int L[100];
-	int R[100];
+	int *B;
+	size_t i;
 
 	if (array == NULL || size < 2)
 		return;
-	middle = size / 2;
-	for (i = 0; i < middle; i++)
-		L[i] = array[i];
-	for (i = middle; i < size; i++)
-		R[i - middle] = array[i];
-	merge_sort(L, middle);
-	merge_sort(R, size - middle);
-	merge(array, L, R, size);
+
+	B = malloc(sizeof(int) * 100);
+	if (B == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		B[i] = array[i];
+	}
+	top_down_split(B, 0, 10, array);
+	free(B);
 }
