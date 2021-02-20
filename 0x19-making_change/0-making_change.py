19486c59d7034795c62882b87a2fb4ebfe7a4bbb
shincap8 <shincap8@gmail.com>
2021-02-20 16:31:44 +0000

    Interview task

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Make Change method"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values, determine
+    the fewest number of coins needed to meet a given amount"""
+    n_coins = 0
+    if total <= 0:
+        return n_coins
+    coins.sort(reverse=True)
+    #en caso de no pasasr runtime en lugar de sortearlo sacar maximo
+    while (total > 0 and coins):
+        n = int(total / coins[0])
+        total = total - (coins[0] * n)
+        n_coins = n_coins + n
+        coins.remove(coins[0])
+    if total != 0:
+        return -1
+    return n_coins
