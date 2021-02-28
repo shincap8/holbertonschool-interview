7fa2912276fdfa953f3f1a878daa01d57f32f787
shincap8 <shincap8@gmail.com>
2021-02-28 19:27:49 +0000

    Upadte edge case

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -15,7 +15,7 @@ List *add_node_end(List **list, char *str)
 	new = malloc(sizeof(List));
 	if (new == NULL)
 		return (NULL);
-	if (list == NULL)
+	if (list == NULL || str == NULL)
 		return (NULL);
 	new->str = str;
 	if (*list == NULL)
@@ -47,7 +47,7 @@ List *add_node_begin(List **list, char *str)
 	new = malloc(sizeof(List));
 	if (new == NULL)
 		return (NULL);
-	if (list == NULL)
+	if (list == NULL || str == NULL)
 		return (NULL);
 	new->str = str;
 	if (*list == NULL)
